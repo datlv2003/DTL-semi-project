c6f2fc4730240c1429ec3780eadacd3c9078ae96
datlv2003 <115679673+datlv2003@users.noreply.github.com>
2023-08-16 16:57:01 +0700

    Update complete.blade.php

--- resources/views/frontend/complete.blade.php
+++ resources/views/frontend/complete.blade.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/complete.css">
 	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script >
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" >
 	<script type="text/javascript">
